710f1543ee98a06a4a619cd2a5efcf123f5aa065
zqiu <zqiu@yelp.com>
2017-11-16 11:34:59 -0800

    Minimal playground for hellow world

--- Application/views.py
+++ Application/views.py
@@ -6,5 +6,4 @@ app = Flask('weibot', static_folder="Application/static", template_folder="Appli
 # index page
 @app.route("/")
 def show_index_page():
-    #return render_template("index.html")
-    return 'hello world'
+    return render_template("index.html")
